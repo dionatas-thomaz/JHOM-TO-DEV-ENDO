526b5245f5731014eb74a1c6d645095c411a9c16
dionatas-thomaz <diontassanto0@gmail.com>
2023-09-03 22:56:07 -0300

    adc

---
+++ ads1/logica-programacao/atv-salario.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+float salario_min , hrs_trab,bruto,porc, salario_a_recb;
+
+printf("digite o salario minino:");
+scanf("%f", &salario_min);
+
+printf("digite a s horas trabalhadas:");
+scanf ("%f", &hrs_trab);
+
+bruto= (salario_min/2)*hrs_trab;
+porc= bruto*0.03;
+salario_a_recb= bruto-porc;
+printf("o salario total sera: %.1f", salario_a_recb);
+return 0;
+}
