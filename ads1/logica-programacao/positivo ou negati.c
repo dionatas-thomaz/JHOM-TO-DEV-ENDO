d74f98efbc6f7d0adfa8db6429d2e8bbaa69ede3
dionatas-thomaz <dionatassanto0@gmail.com>
2023-09-13 09:42:21 -0300

    adc

---
+++ ads1/logica-programacao/positivo ou negati.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+int x;
+
+printf("digite o valor:");
+scanf("%d",& x);
+
+if (x>=1){
+ printf("positivo");
+}else if (x<0){
+printf("negativo");
+}else
+
+printf("nulo");
+  return 0;
+}
