0cfcdd2b5b756541f2f1eddfc5498a3d179f95a5
dionatas-thomaz <dionatassanto0@gmail.com>
2023-09-25 13:02:09 -0300

    adc

---
+++ ads1/logica-programacao/atv12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a;
+
+    printf("digite o numero: ");
+    scanf("%d", &a);
+
+    for (size_t i = 100; i > 0; i--)
+    {
+        if (i%a== 0)
+        {
+            printf("numero div: %d\n", i);
+        }
+    }
+
+    return 0;
+}
