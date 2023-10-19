546b0949544fb6e4a30d8191e4f17f405dfff985
dionatas-thomaz <dionatassanto0@gmail.com>
2023-10-18 21:06:09 -0300

    adc

---
+++ ads1/logica-programacao/Linguagem C/atv30.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+int a ,b ,c;
+
+printf("digite o primeiro digito: ");
+scanf("%d",&a);
+printf("digite o segundo digito: ");
+scanf("%d",&b);
+printf("digite o terceiro digito: ");
+scanf("%d",&c);
+
+int invert= (a*100 + b*10 + c ) + (c*100+b*10+a);
+int posi = ((a+c)*1)+((b+b)*2) + ((c+a)*3);
+int veri = posi%10;
+printf(" inversao soma: %d\n",invert);
+printf(" iposicao: %d\n",posi);
+printf(" numero verificador: %d\n",veri);
+
+    return 0;
+}
