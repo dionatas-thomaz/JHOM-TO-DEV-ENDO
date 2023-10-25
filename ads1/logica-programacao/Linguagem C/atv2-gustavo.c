a258d22e1cb311e912e0c5c295a33b530ff31d2e
dionatas-thomaz <dionatassanto0@gmail.com>
2023-10-24 23:50:19 -0300

    adc

--- ads1/logica-programacao/Linguagem C/atv2-gustavo.c
+++ ads1/logica-programacao/Linguagem C/atv2-gustavo.c
@@ -4,42 +4,43 @@
 
 int main()
 {
-    float a;
+    double a;
     int cont100 = 0, cont50 = 0, cont20 = 0, cont10 = 0, cont5 = 0, cont2 = 0, cont1 = 0;
     int conti5 = 0, conti25 = 0, conti10 = 0, conti05 = 0, conti01 = 0;
 
     printf("Digite um numero: ");
-    scanf("%f", &a);
+    scanf("%lf", &a);
 
     if (a >= 100)
     {
-        cont100 = a / 100;
-        a -= cont100 * 100;
+        cont100 = a / 100.0;
+        a -= cont100 * 100.0;
+        
     }
     if (a >= 50)
     {
-        cont50 = a / 50;
-        a -= cont50 * 50;
+        cont50 = a / 50.0;
+        a -= cont50 * 50.0;
     }
     if (a >= 20)
     {
-        cont20 = a / 20;
-        a -= cont20 * 20;
+        cont20 = a / 20.0;
+        a -= cont20 * 20.0;
     }
     if (a >= 10)
     {
-        cont10 = a / 10;
-        a -= cont10 * 10;
+        cont10 = a / 10.0;
+        a -= cont10 * 10.0;
     }
     if (a >= 5)
     {
-        cont5 = a / 5;
-        a -= cont5 * 5;
+        cont5 = a / 5.0;
+        a -= cont5 * 5.0;
     }
     if (a >= 2)
     {
-        cont2 = a / 2;
-        a -= cont2 * 2;
+        cont2 = a / 2.0;
+        a -= cont2 * 2.0;
     }
     if (a >= 1)
     {
@@ -67,8 +68,7 @@ int main()
     }
     if (a >= 0.01)
     {
-        conti01 = a / 0.01;
-       /* conti01++;*/
+        conti01 =(float)(a *100/1.0);
     }
 
     printf("Cont100: %d\n", cont100);
