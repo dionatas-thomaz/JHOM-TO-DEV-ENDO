e2755c88805394d77200c3eb3ed939c50aa054b9
dionatas-thomaz <dionatassanto0@gmail.com>
2023-11-06 18:08:29 -0300

    adc

---
+++ ads1/logica-programacao/Linguagem C/atv10gustavo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float anos, a = 0, cidadeA, b = 0, cidadeB;
+    printf("digite a populaçao da cidade A: ");
+    scanf("%f", &cidadeA);
+    printf("digite a populaçao da cidade B: ");
+    scanf("%f", &cidadeB);
+    do
+    {
+        a = cidadeA * 0.03;
+        cidadeA += a;
+        b = cidadeB * 0.015;
+        cidadeB += b;
+        anos++;
+    } while (cidadeA <= cidadeB);
+    printf("tamanho da populaçao da cidade A :%.f", cidadeA);
+    printf("\ntamanho da populaçao da cidade B :%.f\n\n", cidadeB);
+    printf("%.f anos", anos);
+
+    return 0;
+}
