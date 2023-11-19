082758afdb34b9691c96c4adaa7846360fc32222
dionatas-thomaz <dionatassanto0@gmail.com>
2023-11-19 12:20:07 -0300

    adc

--- ads1/logica-programacao/Linguagem C/atv18gustavo.c
+++ ads1/logica-programacao/Linguagem C/atv18gustavo.c
@@ -10,13 +10,9 @@ printf("digite o valor de s :");
 scanf("%f",&s);
 for (size_t i = 2; i < n; i++)
 {
-    res+=(s/pow(i,i));
+   
 }
-res+=s;
-res+= 1/pow(n,n);
-n-=1;
-res+=1/pow(n,n);
-printf("resultado:%f",res);
+
 
 return 0;
 }
