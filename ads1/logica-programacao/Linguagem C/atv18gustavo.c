29c3ed3121476bcba5e2a65a008f533db7acdfdb
dionatas-thomaz <dionatassanto0@gmail.com>
2023-11-20 15:46:49 -0300

    adc

--- ads1/logica-programacao/Linguagem C/atv18gustavo.c
+++ ads1/logica-programacao/Linguagem C/atv18gustavo.c
@@ -6,11 +6,11 @@ int main (){
 float n,DOIDO=0;
 printf("digite o valor de N .-. :");
 scanf("%f",&n);
+DOIDO+=n;
 for (size_t i = 2; i < n; i++)
 {
    DOIDO+= (n-1)/pow(i,i);
 }
-DOIDO+=n;
 DOIDO+= 1/pow(n,n);
 n-=1;
 DOIDO+=2/pow(n,n);
