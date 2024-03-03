396dac0e678d6be588363a9b5e84c770822acb3d
dionatas-thomaz <dionatassanto0@gmail.com>
2024-03-03 06:20:32 -0300

    adc

---
+++ ads1/logica-programacao/Linguagem C/bino.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    // Lendo a entrada do exercício
+	int B, C;
+	scanf("%d",&B);
+	scanf("%d",&C);
+
+    // Seu código vai aqui
+B+=C;
+	if(B%2==0)
+		printf("bino");
+	 else 
+		printf("cino");
+	
+		
+}
+
