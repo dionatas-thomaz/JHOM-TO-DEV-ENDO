aaa51d2f7681ff257b0e62c69a3aa6885df40b1e
dionatas-thomaz <dionatassanto0@gmail.com>
2024-07-19 12:55:59 -0300

    adc

--- ads1/logica-programacao/Linguagem C/gusta11.c
+++ ads1/logica-programacao/Linguagem C/gusta11.c
@@ -9,24 +9,25 @@ int main(){
     
  
   
-    int Prova1[TAM],Prova2[TAM];
-   float media[TAM];
+    
+   float media[TAM],Prova1[TAM],Prova2[TAM];
     srand(time(NULL));
     
     for (int i = 0; i < TAM; i++) {
         Prova1[i] = rand() % 10;
         Prova2[i] = rand() % 10;
+    //     printf("vetor[%d] prova 1: %d e prova 2: %d\n", i,Prova1[i],Prova2[i] );
     }
      for (int i = 0; i <= TAM; i++){
          
-         media[i]= (Prova1[i]+Prova2[2])/2;
+         media[i]= (Prova1[i]+Prova2[i])/2;
          
      }
      for (int i = 0; i <= TAM; i++){
          if(media[i]>=6){
-             printf("aluno:%s\n nota da primeira prova: %d\n nota da segunda prova: %d\n media:%.2f situação: aprovado\n",Nomes[i],Prova1[i],Prova2[i],media[i]);
+             printf("aluno:%s\n nota da primeira prova: %.2f\n nota da segunda prova: %.2f\n media:%.2f situação: aprovado\n",Nomes[i],Prova1[i],Prova2[i],media[i]);
          }else{
-              printf("aluno:%s\n nota da primeira prova: %d\n nota da segunda prova: %d\n media:%.2f situação: reprovado\n",Nomes[i],Prova1[i],Prova2[i],media[i]);
+              printf("aluno:%s\n nota da primeira prova: %.2f\n nota da segunda prova: %.2f\n media:%.2f situação: reprovado\n",Nomes[i],Prova1[i],Prova2[i],media[i]);
          }
      }
      
