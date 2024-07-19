5d011bf7d53393e103e40a645487a4d43a655266
dionatas-thomaz <dionatassanto0@gmail.com>
2024-07-19 12:45:55 -0300

    atv

---
+++ ads1/logica-programacao/Linguagem C/gusta11.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+#define TAM 15
+
+ 
+ char *Nomes[]={"Ana","Bruno","Carlos","Daniela","Eduardo","Fernanda","Gabriel","Helena","Igor","Juliana","Lucas","Mariana","Nuno","Olivia","Pedro"};
+
+int main(){
+    
+ 
+  
+    int Prova1[TAM],Prova2[TAM];
+   float media[TAM];
+    srand(time(NULL));
+    
+    for (int i = 0; i < TAM; i++) {
+        Prova1[i] = rand() % 10;
+        Prova2[i] = rand() % 10;
+    }
+     for (int i = 0; i <= TAM; i++){
+         
+         media[i]= (Prova1[i]+Prova2[2])/2;
+         
+     }
+     for (int i = 0; i <= TAM; i++){
+         if(media[i]>=6){
+             printf("aluno:%s\n nota da primeira prova: %d\n nota da segunda prova: %d\n media:%.2f situação: aprovado\n",Nomes[i],Prova1[i],Prova2[i],media[i]);
+         }else{
+              printf("aluno:%s\n nota da primeira prova: %d\n nota da segunda prova: %d\n media:%.2f situação: reprovado\n",Nomes[i],Prova1[i],Prova2[i],media[i]);
+         }
+     }
+     
+   
+    return 0;
+}
