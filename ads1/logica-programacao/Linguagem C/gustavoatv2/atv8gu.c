0a064f778480f7a86d778d90cae5e0c5c2e0aa29
dionatas-thomaz <dionatassanto0@gmail.com>
2024-08-07 18:00:31 -0300

    adc

---
+++ ads1/logica-programacao/Linguagem C/gustavoatv2/atv8gu.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_DADOS 2
+#define MAX_LENGTH 100
+#define DATE_LENGTH 12
+#define ANOS_LENGTH 5
+
+typedef struct
+{
+    char nomes[MAX_DADOS][MAX_LENGTH];
+    char sangue[MAX_DADOS][MAX_LENGTH];
+    char data[MAX_DADOS][DATE_LENGTH];
+    char ano[MAX_DADOS][ANOS_LENGTH];
+} Dados;
+
+int main()
+{
+    Dados dados;
+
+
+return 0;
+}
